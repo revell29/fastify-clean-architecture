e86d925421314bfe30ed108ff75e3a5717efc0dd
apsya Dira <diraapsya0@gmail.com>
2022-03-08 14:30:16 +0700

    refactor: update husky

--- .commitlintrc.js
+++ .commitlintrc.js
@@ -1,1 +1,12 @@
-module.exports = { extends: ['@commitlint/config-conventional'] }
+module.exports = {
+  extends: ['@commitlint/config-conventional'],
+  rules: {
+    //   TODO Add Scope Enum Here
+    // 'scope-enum': [2, 'always', ['yourscope', 'yourscope']],
+    'type-enum': [
+      2,
+      'always',
+      ['feat', 'fix', 'docs', 'chore', 'style', 'refactor', 'ci', 'test', 'perf', 'revert', 'vercel'],
+    ],
+  },
+}
